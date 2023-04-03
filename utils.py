42051cce35bf5a07d7598bd4a955ad97d08eedaa
rcnegby <rcnegby@mail.ru>
2023-04-03 17:51:46 +0300

    add calculate map@K

--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ def get_coo_matrix(df: pd.DataFrame, users_mapping: dict, items_mapping: dict,
     '''
     transfotm pandas df to sparse matrix
     '''
+
     interaction_matrix = coo_matrix((
         np.ones(len(df), dtype=np.float32),
         (
@@ -16,3 +17,23 @@ def get_coo_matrix(df: pd.DataFrame, users_mapping: dict, items_mapping: dict,
         )
     ))
     return interaction_matrix.tocsr()
+
+
+def calc_MAP(test_df: pd.DataFrame, recs: pd.DataFrame):
+    '''
+    calculate MAP@K
+    test_df should contain columns ['user_id', 'item_id']
+    recs should contain columns ['user_id', 'item_id', 'rank']
+    test_df stores interactions
+    recs stores recommended objects with rank for each user
+    '''
+
+    test_recs = test_df.merge(recs, on=['user_id', 'item_id'], how='left')
+    test_recs = test_recs.sort_values(by=['user_id', 'rank'])
+
+    test_recs['users_item_count'] = test_recs.groupby(by='user_id')['rank'].transform(np.size)
+    test_recs['precision@K'] = test_recs.groupby(by='user_id').cumcount() + 1
+    test_recs['precision@K'] = test_recs['precision@K'] / test_recs['rank']
+
+    users_count = test_recs.user_id.nunique()
+    return (test_recs['precision@K'] / test_recs['users_item_count']).sum() / users_count
